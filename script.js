4fb1a0825525b3271ef8e598d9c1fad2e40e2d88
Koustubha068 <KoustuK@gmail.com>
2023-12-08 09:29:00 -0500

    added js

---
+++ script.js
@@ -0,0 +1,9 @@
+let adjective = prompt('Enter an Adjective');
+let noun = prompt('Enter a Noun');
+let adverb = prompt('Enter an Adverb');
+let verb = prompt('Enter a Verb');
+
+let story=`Once upon a time, in a ${adjective} ${noun},a ${verb} ${noun} ${adverb}  ${verb}.`
+let madLibOutputDiv = document.getElementById('madLibOutput');
+
+madLibOutputDiv.innerHTML =`<p>${story}</p>`
