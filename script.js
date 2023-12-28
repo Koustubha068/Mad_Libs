59f2c82294bfab15fcac9139b5155cb3365341dd
Koustubha068 <144465165+Koustubha068@users.noreply.github.com>
2023-12-27 20:40:09 -0500

    changed order of variables

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+
+let adjective = prompt('Enter a adjective');
 let place = prompt('Enter a noun(place)');
 let person = prompt('Enter a noun(name)');
-let noun = prompt('Enter a Noun');
-let Noun = prompt('Enter a plural noun');
-let adverb = prompt('Enter an Adverb');
-let verb = prompt('Enter a Verb');
 let adjective = prompt('Enter a adjective');
+let verb = prompt('Enter a Verb');
+let Noun = prompt('Enter a plural noun');
+let verb = prompt('Enter a Verb');
+let verb = prompt('Enter a Verb');
+let noun = prompt('Enter a Noun');
+let verb = prompt('Enter a Verb');
 
 
 let story=`In the ${adjective} town of ${place}, ${person}, a ${adjective} snowman, ${verb} a plan to have a snow ball fight. The snowman assembled many ${Noun} which were able to ${verb} and ${verb} several snowballs at the opposing ${noun}. At the end of the day, the snowman's team won. The town became a very ${verb} place due to the victory.`
