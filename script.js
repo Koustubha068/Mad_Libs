c632a424b91c0f9140ea02a596b6c8f280b7272a
Koustubha068 <144465165+Koustubha068@users.noreply.github.com>
2023-12-27 20:22:24 -0500

    changed story

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let adjective = prompt('Enter an Adjective');
+let place = prompt('Enter a noun(place)');
+let person = prompt('Enter a noun(name)');
 let noun = prompt('Enter a Noun');
+let Noun = prompt('Enter a plural noun');
 let adverb = prompt('Enter an Adverb');
 let verb = prompt('Enter a Verb');
+let adjective = prompt('Enter a adjective');
 
-let story=`Once upon a time, in a ${adjective} ${noun},a ${verb} ${noun} ${adverb}  ${verb}.`
+
+let story=`In the ${adjective} town of ${place}, ${person}, a ${adjective} snowman, ${verb} a plan to have a snow ball fight. The snowman assembled many ${Noun} which were able to ${verb} and ${verb} several snowballs at the opposing ${noun}. At the end of the day, the snowman's team won. The town became a very ${verb} place due to the victory.`
 let madLibOutputDiv = document.getElementById('madLibOutput');
 
 madLibOutputDiv.innerHTML =`<p>${story}</p>`
